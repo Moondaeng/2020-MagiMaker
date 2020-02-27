7ab185a8031f8e3511f3b87663e741ba4829a64d
SoMuL <evenine9@gmail.com>
2020-02-27 23:35:32 +0900

    move시 자기 자신도 msg 수신하는 코드 수정

--- MagiMakerServer/MagiMakerServer/PacketProc.cpp
+++ MagiMakerServer/MagiMakerServer/PacketProc.cpp
@@ -84,7 +84,7 @@ BOOL MoveStart(Session *session, Packet &p)
 
 	MakePacketMoveStart(sendPacket, session->sessionID, nowX, nowY, destX, destY);
 	
-	SendPacketBroadcast(session,sendPacket);
+	SendPacketBroadcast(session,sendPacket,FALSE);
 
 	return TRUE;
 }
@@ -105,7 +105,7 @@ BOOL MoveStop(Session *session, Packet &p)
 
 	MakePacketMoveStop(sendPacket, session->sessionID, nowX, nowY);
 
-	SendPacketBroadcast(session,sendPacket);
+	SendPacketBroadcast(session,sendPacket,FALSE);
 
 	return TRUE;
 }
