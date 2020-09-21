9bc62e85ba0598502f1169c1baaae1bd6f7c0fdf
ZeroFe <skel2cha@gmail.com>
2020-09-21 23:13:37 +0900

    아이템 드랍 테이블 및 아이템 매니저(아이템 정보 관리 클래스) 제작

--- Item/CItemInformation.cs
+++ Item/CItemInformation.cs
@@ -11,86 +11,86 @@ public class CItemInformation : MonoBehaviour
     void Start()
     {
         _item = new Item.CItem();
-        GetItemInfoFromItemManager();
+        //GetItemInfoFromItemManager();
         _item.Print();
         _goitem.GetComponent<Rigidbody>().AddForce(new Vector3(0, 30, 0) * 10f); //물체 한번 띄우기용
     }
 
-    void GetItemInfoFromItemManager()
-    {
+    //void GetItemInfoFromItemManager()
+    //{
 
-        int itemCode;
-        int random;
-        int itemType = 1;
-        System.Random r = new System.Random();
+    //    int itemCode;
+    //    int random;
+    //    int itemType = 1;
+    //    System.Random r = new System.Random();
 
-        if (false) //소비템의 경우 조건문으로 나오게하기
-            itemType = 0;
+    //    if (false) //소비템의 경우 조건문으로 나오게하기
+    //        itemType = 0;
 
 
-        switch (itemType)
-        {
-            case 0: //소비형
-                random = r.Next() % 100;
-                //일단 확률 흔함40, 특별30, 희귀20, 신비10 으로 설정
-                if (random >= 0 && random < CConstants.COMMON)
-                {
-                    itemCode = 0000;
-                    _item = CItemManager.instance.GetItemInfo(itemCode);
-                }
-                else if (random >= CConstants.COMMON && random < CConstants.SPECIAL)
-                {
-                    itemCode = 1000;
-                    _item = CItemManager.instance.GetItemInfo(itemCode);
-                }
-                else if (random >= CConstants.SPECIAL && random < CConstants.UNIQUE)
-                {
-                    itemCode = 2000;
-                    _item = CItemManager.instance.GetItemInfo(itemCode);
-                }
-                else //신비등급 필요하면 elseif로 변경
-                {
-                    itemCode = 3000;
-                    _item = CItemManager.instance.GetItemInfo(itemCode);
-                }
-                break;
-            case 1: //장착형
-                while (true)
-                {
-                    random = r.Next() % 100;
-                    //일단 확률 흔함40, 특별30, 희귀20, 신비10 으로 설정
-                    if (random >= 0 && random < CConstants.COMMON)
-                    {
-                        itemCode = 0100;
-                        _item = CItemManager.instance.GetItemInfo(itemCode);
-                    }
-                    else if (random >= CConstants.COMMON && random < CConstants.SPECIAL)
-                    {
-                        itemCode = 1100;
-                        _item = CItemManager.instance.GetItemInfo(itemCode);
-                    }
-                    else if (random >= CConstants.SPECIAL && random < CConstants.UNIQUE)
-                    {
-                        itemCode = 2100;
-                        _item = CItemManager.instance.GetItemInfo(itemCode);
-                    }
-                    else //신비등급 필요하면 elseif로 변경
-                    {
-                        itemCode = 3100;
-                        _item = CItemManager.instance.GetItemInfo(itemCode);
-                    }
-                    //나올템이 더 없는 지 체크 있으면 반복 없으면 종료, 장착형인 경우에만 확인
-                    if (_item.ItemName == "nothing")
-                    {
-                        if (CItemManager.instance.IsEmptyWithoutShopItem())
-                            break;
-                    }
-                    else
-                        break;
-                }
-                break;
-        }
-    }
+    //    switch (itemType)
+    //    {
+    //        case 0: //소비형
+    //            random = r.Next() % 100;
+    //            //일단 확률 흔함40, 특별30, 희귀20, 신비10 으로 설정
+    //            if (random >= 0 && random < CConstants.COMMON)
+    //            {
+    //                itemCode = 0000;
+    //                _item = CItemManager.instance.GetItemInfo(itemCode);
+    //            }
+    //            else if (random >= CConstants.COMMON && random < CConstants.SPECIAL)
+    //            {
+    //                itemCode = 1000;
+    //                _item = CItemManager.instance.GetItemInfo(itemCode);
+    //            }
+    //            else if (random >= CConstants.SPECIAL && random < CConstants.UNIQUE)
+    //            {
+    //                itemCode = 2000;
+    //                _item = CItemManager.instance.GetItemInfo(itemCode);
+    //            }
+    //            else //신비등급 필요하면 elseif로 변경
+    //            {
+    //                itemCode = 3000;
+    //                _item = CItemManager.instance.GetItemInfo(itemCode);
+    //            }
+    //            break;
+    //        case 1: //장착형
+    //            while (true)
+    //            {
+    //                random = r.Next() % 100;
+    //                //일단 확률 흔함40, 특별30, 희귀20, 신비10 으로 설정
+    //                if (random >= 0 && random < CConstants.COMMON)
+    //                {
+    //                    itemCode = 0100;
+    //                    _item = CItemManager.instance.GetItemInfo(itemCode);
+    //                }
+    //                else if (random >= CConstants.COMMON && random < CConstants.SPECIAL)
+    //                {
+    //                    itemCode = 1100;
+    //                    _item = CItemManager.instance.GetItemInfo(itemCode);
+    //                }
+    //                else if (random >= CConstants.SPECIAL && random < CConstants.UNIQUE)
+    //                {
+    //                    itemCode = 2100;
+    //                    _item = CItemManager.instance.GetItemInfo(itemCode);
+    //                }
+    //                else //신비등급 필요하면 elseif로 변경
+    //                {
+    //                    itemCode = 3100;
+    //                    _item = CItemManager.instance.GetItemInfo(itemCode);
+    //                }
+    //                //나올템이 더 없는 지 체크 있으면 반복 없으면 종료, 장착형인 경우에만 확인
+    //                if (_item.ItemName == "nothing")
+    //                {
+    //                    if (CItemManager.instance.IsEmptyWithoutShopItem())
+    //                        break;
+    //                }
+    //                else
+    //                    break;
+    //            }
+    //            break;
+    //    }
+    //}
 }
 
 
--- Test/CTestContorller.cs
+++ Test/CTestContorller.cs
@@ -17,6 +17,11 @@ public class CTestContorller : MonoBehaviour
         // 조작 관리
         keyDictionary = new Dictionary<KeyCode, Action>
         {
+            {KeyCode.KeypadPlus, () => {
+                    var item = CItemDropTable.instance.PopRandomItemByGrade(CItemDropTable.ItemGrade.Normal);
+                    item.SetActive(true);
+                    item.transform.position = GetHitPoint();                    
+                } },
             {KeyCode.U, () => commander.SetMyCharacter(0) },
             {KeyCode.I, () => commander.SetMyCharacter(1) },
             {KeyCode.O, () => commander.SetMyCharacter(2) },
@@ -40,31 +45,6 @@ public class CTestContorller : MonoBehaviour
             //{KeyCode.L, () => waiting.SetPlayerAccept(3, CWaitingForAccept.EAccept._cancel) }
         };
     }
-
-//    private void Awake()
-//    {
-//        // 조작 관리
-//        keyDictionary = new Dictionary<KeyCode, Action>
-//        {
-//            {KeyCode.Alpha1, () => commander.SetMyCharacter(0) },
-//            //{KeyCode.Alpha2, () => commander.SetMyCharacter(1) },
-//            //{KeyCode.Alpha3, () => commander.SetMyCharacter(2) },
-//            //{KeyCode.Alpha4, () => commander.SetActivePlayers(4) },
-//            //{KeyCode.Alpha5, () => commander.UseSkill(1, 0,  GetHitPoint()) },
-//            {KeyCode.Insert, () => commander.SetActivePlayers(4) },
-//            {KeyCode.Alpha9, () => commander.Move(1, GetHitPoint()) },
-//            {KeyCode.Alpha0, () => commander.Move(2, GetHitPoint()) },
-//            {KeyCode.Delete, () => Destroy(GameObject.FindGameObjectWithTag("ITEM")) },
-//            {KeyCode.M, () => commander.Teleport(0, new Vector3(0.09f, 0.61f, 80.0f)) },
-//            {KeyCode.U, () => waiting.SetPlayerAccept(1, CWaitingForAccept.EAccept._accept) },
-//            {KeyCode.I, () => waiting.SetPlayerAccept(2, CWaitingForAccept.EAccept._accept) },
-//            {KeyCode.O, () => waiting.SetPlayerAccept(3, CWaitingForAccept.EAccept._accept) },
-//            {KeyCode.J, () => waiting.SetPlayerAccept(1, CWaitingForAccept.EAccept._cancel) },
-//            {KeyCode.K, () => waiting.SetPlayerAccept(2, CWaitingForAccept.EAccept._cancel) },
-//            {KeyCode.L, () => waiting.SetPlayerAccept(3, CWaitingForAccept.EAccept._cancel) }
-
-        //        };
-        //    }
 
     private void Update()
     {
