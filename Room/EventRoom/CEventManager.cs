7ea726b2964154b42b79baae1d4182fb110f4e3a
rlatjrfo57 <rlatjrfo57@gmail.com>
2021-01-29 15:34:46 +0900

    이벤트 매니저 폴더 변경

---
+++ Room/EventRoom/CEventManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CEventManager : MonoBehaviour
+{
+    public UnityEvent _eventEvent;
+
+    public static CEventManager _instance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (_instance == null)
+            _instance = this;
+    }
+
+    public void InvokeEvent()
+    {
+        _eventEvent.Invoke();
+    }
+}
