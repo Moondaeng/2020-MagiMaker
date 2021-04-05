e2cef91d7d554cca10008f889048e4855a4d1b27
rlatjrfo57 <rlatjrfo57@gmail.com>
2021-04-05 14:24:14 +0900

    포탈 + 보상npc와 같이 이동하도록 수정

--- Portal/CItemEliteRoomPortal.cs
+++ Portal/CItemEliteRoomPortal.cs
@@ -5,28 +5,34 @@ using UnityEngine;
 public class CItemEliteRoomPortal : CPortal
 {
     private Vector3 _presentPosition;
-    private Transform _portalMom;
+    private Vector3 _treasurePresentPosition;
+    private Transform _portalGroup;
+    private Transform _treasure;
 
     public override void OpenNClosePortal()
     {
         Debug.Log("before move position");
 
-        _portalMom = transform.parent;
+        _portalGroup = transform.parent.parent;
+        _treasure = GameObject.FindGameObjectWithTag("NPC").transform;
 
         if (!CGlobal.isClear) //클리어 전에는 포탈 없애야함.
         {
-            if (_portalMom.position == new Vector3(1000, 1000, 1000)) //중복해서 호출되는 경우 방지
+            if (_portalGroup.position == new Vector3(1000, 1000, 1000)) //중복해서 호출되는 경우 방지
                 return;
 
-            _presentPosition = _portalMom.position; //현재 위치 저장해두기
-            _portalMom.position = new Vector3(1000, 1000, 1000); //저 멀리 던져두기
+            _presentPosition = _portalGroup.position; //현재 위치 저장해두기
+            _treasurePresentPosition = _treasure.position;
+            _portalGroup.position = new Vector3(1000, 1000, 1000); //저 멀리 던져두기
+            _treasure.position = new Vector3(1000, 1000, 1000);
 
             Debug.Log("move position");
         }
         else // 클리어
         {
             Debug.Log("return position");
-            _portalMom.position = _presentPosition; //다시 가져오기
+            _portalGroup.position = _presentPosition; //다시 가져오기
+            _treasure.position = _treasurePresentPosition;
         }
 
     }
